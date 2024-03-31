46ec0ca4bb57c8e8bf7d62538932f70dab739d87
Philipp Engelhard <philippengelhard@gmail.com>
2024-03-31 17:44:41 +0200

    Renamed to Icosphere

--- register_types.cpp
+++ register_types.cpp
@@ -4,7 +4,7 @@
 #include "icosphere.h"
 
 
-void initialize_icooctree_module(ModuleInitializationLevel p_level) {
+void initialize_icosphere_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
@@ -12,7 +12,7 @@ void initialize_icooctree_module(ModuleInitializationLevel p_level) {
 	GDREGISTER_CLASS(Icosphere);
 }
 
-void uninitialize_icooctree_module(ModuleInitializationLevel p_level) {
+void uninitialize_icosphere_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
--- register_types.h
+++ register_types.h
@@ -1,9 +1,9 @@
-#ifndef ICOOCTREE_REGISTER_TYPES_H
-#define ICOOCTREE_REGISTER_TYPES_H
+#ifndef ICOSPHERE_REGISTER_TYPES_H
+#define ICOSPHERE_REGISTER_TYPES_H
 
 #include "modules/register_module_types.h"
 
-void initialize_icooctree_module(ModuleInitializationLevel p_level);
-void uninitialize_icooctree_module(ModuleInitializationLevel p_level);
+void initialize_icosphere_module(ModuleInitializationLevel p_level);
+void uninitialize_icosphere_module(ModuleInitializationLevel p_level);
 
-#endif // ICOOCTREE_REGISTER_TYPES_H
+#endif // ICOSPHERE_REGISTER_TYPES_H
